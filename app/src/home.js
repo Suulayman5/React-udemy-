798e28f9d040cd2e08ced79e5a6e00f2a6f170ca
ajah suleiman <ajahsuleiman@gmail.com>
2024-06-22 13:59:20 +0100

    usestate

--- app/src/home.js
+++ app/src/home.js
@@ -1,14 +1,41 @@
-const Home = () => {
-    return ( 
-        <div className="Home">
-            <h2>Homepage</h2><br/>
-            {/* <Example/> */}
-            <Example2 Name = 'Sly' Age= '32' Job= 'Front-end dev'/>
-            <Example2 Name = 'mily' Age= '22' Job= 'Back-end dev'/>
-            <Example2 Name = 'Morenike' Age= '22' Job= 'Olosho'/>
-            <Example2 Name ='Ajoke' Age= '29' Job= 'Snr-Olosho'/>
+import { useState } from "react";
 
+const Home = () => {
+    // const [name, setName] = useState('john')
+    
+    // const handleClick =()=>{
+    //     setName('papa')
+    const [count, setCount]= useState(0)
+    function incrateCount(){
+        setCount(count + 1)
+    }
+    
+    function decreaseCount(){
+        setCount(count - 1)
+    }
+    return ( 
+        <div className='home'>
+            <h2>Homepage</h2>
+            <button onClick={incrateCount}>CLICK CLAK</button><br/>
+            <p>count is {count}</p><br/>
+            <button onClick={decreaseCount}>CLICK CLAK</button>
+        {/* <button onClick={handleClick}>Cliky</button>
+        <p>{name}</p> */}
         </div>
+
+        // <div className="Home">
+        //     <h2>Homepage</h2><br/><hr/>
+        //     {/* <Example/> */}
+        //     <Example2 Name = 'Sly' Age= '32' Job= 'Front-end dev'/>
+        //     <Example2 Name = 'mily' Age= '22' Job= 'Back-end dev'/>
+        //     <Example2 Name = 'Morenike' Age= '22' Job= 'Olosho'/>
+        //     <Example2 Name ='Ajoke' Age= '29' Job= 'Snr-Olosho'/>
+
+        //     <Book Title = 'Tap Tap' Author = 'Suleiman Dickson'  Description= 'Learn about tap currency' Date = '22nd June 2024'/>
+
+        // </div>
+    
+
      );
     }
 const  Example2= (props) => {
@@ -16,11 +43,23 @@ const  Example2= (props) => {
         <div>
             <h3>Name: {props.Name}</h3><br/>
             <h3>Age: {props.Age}</h3><br/>
-            <h3>Jobs: {props.Job}</h3><br/><hr/>
+            <h3>Job: {props.Job}</h3><br/><hr/>
         </div>
      );
 }
  
+const Book = (props) => {
+    return ( 
+        <p>
+            <h2>Title: {props.Title}</h2><br/>
+            <h3>Author: {props.Author}</h3><br/>
+            <h4>Description: {props.Description}</h4><br/>
+            <h6>Date: {props.Date}</h6><br/>
+        </p>
+     );
+}
+ 
+
 
  
 // const Example = ()=>{
