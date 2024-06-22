b3892260e90934d9d54a4f5eca72107c571c66c5
ajah suleiman <ajahsuleiman@gmail.com>
2024-06-22 08:38:08 +0100

    first

---
+++ app/src/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className = "navbar">
+            <h1>My web app</h1>
+            <div className="links">
+                <a href ="/"></a>
+                <a href ="/create"></a>
+
+            </div>
+        </nav>
+        
+     );
+}
+ 
+export default Navbar;
